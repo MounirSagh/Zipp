84399fa2798b64ea6ee788ab443972d00409c1ea
MounirSagh <mounirsaghfary@gmail.com>
2025-09-16 16:35:29 +0100

    order onsite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Admin from './pages/admin';
 import CuisinePanel from './pages/cuisine';
 import SignIn from './pages/SignIn'
