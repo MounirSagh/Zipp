d391f7b04c23cab8e41d1d105b7e198c46da4d4b
MounirSagh <mounirsaghfary@gmail.com>
2025-09-30 17:51:40 +0100

    UI changes

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -22,15 +22,7 @@ const opacity = {
   },
 };
 
-const slideUp = {
-  initial: {
-    top: 0,
-  },
-  exit: {
-    top: "-100vh",
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 },
-  },
-};
+
 
 interface PreloaderProps {
   onComplete: () => void;
@@ -63,29 +55,9 @@ export default function Preloader({ onComplete }: PreloaderProps) {
     return () => clearTimeout(timeout);
   }, [index, onComplete]);
 
-  const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
-    dimension.height
-  } Q${dimension.width / 2} ${dimension.height + 300} 0 ${
-    dimension.height
-  } L0 0`;
-  const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
-    dimension.height
-  } Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height} L0 0`;
-
-  const curve = {
-    initial: {
-      d: initialPath,
-      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] },
-    },
-    exit: {
-      d: targetPath,
-      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 },
-    },
-  };
 
   return (
     <motion.div
-      variants={slideUp}
       initial="initial"
       exit="exit"
       style={{
@@ -238,25 +210,6 @@ export default function Preloader({ onComplete }: PreloaderProps) {
               </h1>
             </div>
           </div>
-
-          {/* SVG Curve */}
-          <svg
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "calc(100% + 300px)",
-              pointerEvents: "none",
-            }}
-          >
-            <motion.path
-              variants={curve}
-              initial="initial"
-              exit="exit"
-              fill="#141516"
-            />
-          </svg>
         </>
       )}
     </motion.div>
